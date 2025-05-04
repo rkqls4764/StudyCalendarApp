14829a355812a4d0c88348da21d10fd1854fa742
rkqls4764 <rkqls4764@naver.com>
2025-05-04 23:16:18 +0900

    Chore: Google 소셜 로그인 리소스 관련 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,5 @@ plugins {
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
 
     // Add the dependency for the Google services Gradle plugin
-    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.gms.google-services") version "4.3.8" apply false
 }
