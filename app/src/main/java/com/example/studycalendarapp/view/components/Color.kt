32a821b51935e88ffb130187442efcb7b20a3143
rkqls4764 <rkqls4764@naver.com>
2025-05-04 22:40:11 +0900

    Design: 글자 색상 및 버튼 색상 키워드 정의

--- app/src/main/java/com/example/studycalendarapp/view/components/Color.kt
+++ app/src/main/java/com/example/studycalendarapp/view/components/Color.kt
@@ -4,3 +4,6 @@ import androidx.compose.ui.graphics.Color
 
 val MainBlue = Color(0xFF9ECFFF)    // 주 색상
 val SubBlue = Color(0xFF78A8D1)     // 서브 색상
+val MainText = Color(0xFF121417)    // 주 글자 색상
+val SubText = Color(0xFF637887)     // 서브 글자 색상
+val ButtonBack = Color(0xFFF0F2F5)  // 버튼 배경 색
