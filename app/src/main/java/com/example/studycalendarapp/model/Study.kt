d43c4e44bce8225b3e89644bf76f3347e19b9e43
rkqls4764 <rkqls4764@naver.com>
2025-05-05 20:06:47 +0900

    Feat: 스터디 데이터 구조 변경
    방장 아이디 필드, 가입한 사용자 아이디 목록 필드 추가

--- app/src/main/java/com/example/studycalendarapp/model/Study.kt
+++ app/src/main/java/com/example/studycalendarapp/model/Study.kt
@@ -1,10 +1,12 @@
 package com.example.studycalendarapp.model
 
 data class Study(
-    val name: String = "",          // 이름
-    val description: String = "",   // 설명
-    val method: String = "",        // 방식
-    val date: String = "",          // 날짜
-    val time: String = "",          // 시간
-    val tag: String = ""            // 태그
+    val name: String = "",                      // 이름
+    val description: String = "",               // 설명
+    val method: String = "",                    // 방식
+    val date: String = "",                      // 날짜
+    val time: String = "",                      // 시간
+    val tag: String = "",                       // 태그
+    val hostId: String = "",                    // 방장 아이디
+    val memberList: List<String> = emptyList()  // 가입한 사용자 아이디 목록
 )
