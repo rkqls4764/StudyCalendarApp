85a2703fff304ff6b2e0f47349d291255d7cfd3b
rkqls4764 <rkqls4764@naver.com>
2025-05-05 17:59:49 +0900

    Feat: 입력 필드 컴포넌트 생성
    TextField를 컴포넌트로 생성

---
+++ app/src/main/java/com/example/studycalendarapp/view/components/InputRow.kt
@@ -0,0 +1,31 @@
+package com.example.studycalendarapp.view.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun InputField(
+    label: String,
+    value: String,
+    onChange: (String) -> Unit
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Text(label, fontWeight = FontWeight.Bold)
+        TextField(value = value, onValueChange = onChange)
+    }
+    Spacer(modifier = Modifier.height(20.dp))
+}
