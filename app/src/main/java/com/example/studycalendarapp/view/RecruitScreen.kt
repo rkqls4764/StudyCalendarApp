12c68b0517d9e7b7da06aecfcc010f24ffc46df5
rkqls4764 <rkqls4764@naver.com>
2025-05-06 17:01:51 +0900

    Feat: 스터디 이름 및 태그 기준 검색 기능 추가

--- app/src/main/java/com/example/studycalendarapp/view/RecruitScreen.kt
+++ app/src/main/java/com/example/studycalendarapp/view/RecruitScreen.kt
@@ -2,43 +2,65 @@ package com.example.studycalendarapp.view
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.example.studycalendarapp.R
+import com.example.studycalendarapp.view.components.ButtonBack
+import com.example.studycalendarapp.view.components.ButtonBackDeep
 import com.example.studycalendarapp.view.components.MainBlue
 import com.example.studycalendarapp.view.components.StudyBottomNavigationBar
 import com.example.studycalendarapp.view.components.StudyItem
 import com.example.studycalendarapp.view.components.SubBlue
+import com.example.studycalendarapp.viewmodel.JoiningStudyViewModel
+import com.example.studycalendarapp.viewmodel.RecruitViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RecruitScreen(navController: NavHostController) {
-    val searchText = "검색어 예시"
-    val studyList = listOf("스터디1", "스터디2", "스터디3")
+    val viewModel: RecruitViewModel = viewModel()
+    val searchText by viewModel.searchText.collectAsState() // 검색어
+    val filteredList by viewModel.filteredStudyList.collectAsState() // 필터링된 스터디 목록
+
+    LaunchedEffect(Unit) {
+        viewModel.fetchStudyList()
+    }
 
     Scaffold(
         topBar = {
@@ -71,22 +93,32 @@ fun RecruitScreen(navController: NavHostController) {
         }
     ) { paddingValues ->
         Column(
-            modifier = Modifier.padding(paddingValues).padding(top = 30.dp),
+            modifier = Modifier
+                .padding(paddingValues)
+                .padding(top = 18.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Row {   // 검색 바
+            Row(
+                modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) { // 검색 바
                 TextField( // 검색어 입력 필드
                     value = searchText,
-                    onValueChange = { /* viewModel 값 변경 */ }
+                    onValueChange = { viewModel.updateSearchText(it) },
+                    placeholder = {
+                        Text(
+                            text = "검색어를 입력하세요",
+                            fontSize = 16.sp
+                        )
+                    },
+                    trailingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = "검색 버튼"
+                        )
+                    },
+                    modifier = Modifier.fillMaxWidth()
                 )
-                Button( // 검색 버튼
-                    onClick = { /*TODO*/ },
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = Color.Gray
-                    )
-                ) {
-                    Text("검색")
-                }
             }
             // 스터디 목록
             LazyColumn(
@@ -94,12 +126,11 @@ fun RecruitScreen(navController: NavHostController) {
                     .fillMaxSize()
                     .padding(16.dp)
             ) {
-//                items(studyList) { study ->
-//                    StudyItem(study, onButtonClick = {
-//                        // 스터디 참여 화면으로 이동
-//                        navController.navigate("joinStudy")
-//                    })
-//                }
+                items(filteredList) { study ->
+                    StudyItem(study, onButtonClick = {
+                        navController.navigate("joinStudy")  // 스터디 참여 화면으로 이동
+                    })
+                }
             }
         }
     }
