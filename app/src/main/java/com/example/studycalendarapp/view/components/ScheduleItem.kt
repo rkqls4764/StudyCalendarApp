59085c08e83cc9e20769d80e83730ad26b9d069b
rkqls4764 <rkqls4764@naver.com>
2025-05-06 21:29:35 +0900

    Design: 일정 목록 아이템 출력 UI 변경

--- app/src/main/java/com/example/studycalendarapp/view/components/ScheduleItem.kt
+++ app/src/main/java/com/example/studycalendarapp/view/components/ScheduleItem.kt
@@ -4,26 +4,49 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.studycalendarapp.model.Schedule
 
 @Composable
-fun ScheduleItem(name: String, navController: NavController) {
-    Box(
-        modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp)
+fun ScheduleItem(schedule: Schedule, navController: NavController) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 12.dp)
+            .clickable { navController.navigate("detailSchedule") }
     ) {
+        Text(
+            text = schedule.date + " ",
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Bold,
+            color = MainText,
+            modifier = Modifier.width(70.dp)
+        )
+
         Column(
-            modifier = Modifier.fillMaxWidth().clickable { navController.navigate("detailSchedule") },
-            verticalArrangement = Arrangement.Center
+            modifier = Modifier.weight(1f)
         ) {
             Text(
-                text = name,
-                modifier = Modifier.padding(16.dp)
+                text = schedule.name,
+                fontSize = 14.sp,
+                color = MainText
+            )
+
+            Text(
+                text = schedule.time,
+                fontSize = 14.sp,
+                color = SubText
             )
         }
     }
