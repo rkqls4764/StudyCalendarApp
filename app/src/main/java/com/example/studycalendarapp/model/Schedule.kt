a910e336232686b5118214dbe3572f5550f2c23d
rkqls4764 <rkqls4764@naver.com>
2025-05-06 21:33:08 +0900

    Feat: Schedule 데이터 클래스 추가
    일정 이름, 날짜, 시간, 설명 정보를 담는 Schedule 모델 정의

---
+++ app/src/main/java/com/example/studycalendarapp/model/Schedule.kt
@@ -0,0 +1,8 @@
+package com.example.studycalendarapp.model
+
+data class Schedule(
+    val name: String = "",          // 이름
+    val date: String = "",          // 날짜
+    val time: String = "",          // 시간
+    val description: String = ""    // 설명
+)
