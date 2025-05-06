8809266bc811e38cac7d4053db9b64db260201a1
rkqls4764 <rkqls4764@naver.com>
2025-05-06 16:30:14 +0900

    Design: 진한 버튼 배경 색 추가

--- app/src/main/java/com/example/studycalendarapp/view/components/Color.kt
+++ app/src/main/java/com/example/studycalendarapp/view/components/Color.kt
@@ -2,8 +2,9 @@ package com.example.studycalendarapp.view.components
 
 import androidx.compose.ui.graphics.Color
 
-val MainBlue = Color(0xFF9ECFFF)    // 주 색상
-val SubBlue = Color(0xFF78A8D1)     // 서브 색상
-val MainText = Color(0xFF121417)    // 주 글자 색상
-val SubText = Color(0xFF637887)     // 서브 글자 색상
-val ButtonBack = Color(0xFFF0F2F5)  // 버튼 배경 색
+val MainBlue = Color(0xFF9ECFFF)        // 주 색상
+val SubBlue = Color(0xFF78A8D1)         // 서브 색상
+val MainText = Color(0xFF121417)        // 주 글자 색상
+val SubText = Color(0xFF637887)         // 서브 글자 색상
+val ButtonBack = Color(0xFFF0F2F5)      // 버튼 배경 색
+val ButtonBackDeep = Color(0xFFD9D9D9)  // 진한 버튼 배경 색
