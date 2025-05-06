dd6fdd824862e3362da2cb236bbf035cc99b1ecc
rkqls4764 <rkqls4764@naver.com>
2025-05-06 21:55:05 +0900

    Feat: 일정 정보 입력 기능 및 일정 생성 기능 구현

--- app/src/main/java/com/example/studycalendarapp/view/AddScheduleScreen.kt
+++ app/src/main/java/com/example/studycalendarapp/view/AddScheduleScreen.kt
@@ -1,11 +1,14 @@
 package com.example.studycalendarapp.view
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -21,27 +24,33 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.example.studycalendarapp.R
+import com.example.studycalendarapp.view.components.InputField
 import com.example.studycalendarapp.view.components.MainBlue
 import com.example.studycalendarapp.view.components.SubBlue
+import com.example.studycalendarapp.viewmodel.AddScheduleViewModel
+import com.example.studycalendarapp.viewmodel.AddStudyViewModel
 
 /* 일정 추가 화면 */
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AddScheduleScreen(navController: NavHostController) {
-    val name = "이름"
-    val date = "날짜"
-    val time = "시간"
-    val description = "설명"
+fun AddScheduleScreen(navController: NavHostController, studyId: String) {
+    val context = LocalContext.current
+    val viewModel: AddScheduleViewModel = viewModel()
+    val schedule by viewModel.schedule.collectAsState() // 일정 정보
 
     Scaffold(
         topBar = {
@@ -70,60 +79,50 @@ fun AddScheduleScreen(navController: NavHostController) {
         }
     ) { paddingValues ->
         Column(
-            modifier = Modifier.padding(paddingValues).fillMaxWidth().padding(top = 30.dp),
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxWidth()
+                .padding(top = 30.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Row(    // 이름 입력 필드
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceEvenly
-            ) {
-                Text(text = "이름", fontWeight = FontWeight.Bold)
-                TextField(value = name, onValueChange = { /* viewModel 값 변경 */ })
+            InputField("이름", schedule.name) { new ->
+                viewModel.updateSchedule { it.copy(name = new) }
             }
 
-            Spacer(modifier = Modifier.padding(bottom = 20.dp))
-
-            Row(    // 날짜 입력 필드
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceEvenly
-            ) {
-                Text(text = "날짜", fontWeight = FontWeight.Bold)
-                TextField(value = date, onValueChange = { /* viewModel 값 변경 */ })
+            InputField("날짜", schedule.date) { new ->
+                viewModel.updateSchedule { it.copy(date = new) }
             }
 
-            Spacer(modifier = Modifier.padding(bottom = 20.dp))
-
-            Row(    // 시간 입력 필드
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceEvenly
-            ) {
-                Text(text = "시간", fontWeight = FontWeight.Bold)
-                TextField(value = time, onValueChange = { /* viewModel 값 변경 */ })
+            InputField("시간", schedule.time) { new ->
+                viewModel.updateSchedule { it.copy(time = new) }
             }
 
-            Spacer(modifier = Modifier.padding(bottom = 20.dp))
-
-            Row(    // 설명 입력 필드
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceEvenly
-            ) {
-                Text(text = "설명", fontWeight = FontWeight.Bold)
-                TextField(value = description, onValueChange = { /* viewModel 값 변경 */ })
+            InputField("설명", schedule.description) { new ->
+                viewModel.updateSchedule { it.copy(description = new) }
             }
 
             Spacer(modifier = Modifier.padding(bottom = 100.dp))
 
             Button( // 생성 버튼
-                onClick = { /*TODO*/ },
+                onClick = {
+                    viewModel.saveSchedule(
+                        studyId = studyId,
+                        onSuccess = {
+                            Toast.makeText(context, "일정 생성 성공!", Toast.LENGTH_SHORT).show()
+                            navController.popBackStack()   // 뒤로 가기
+                        },
+                        onFailure = {
+                            Toast.makeText(context, "일정 생성 실패", Toast.LENGTH_SHORT).show()
+                        }
+                    )
+                },
                 colors = ButtonDefaults.buttonColors(
                     containerColor = SubBlue
-                )
+                ),
+                shape = RoundedCornerShape(10.dp),
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 30.dp).height(50.dp)
             ) {
-                Text(text = "생성하기", fontWeight = FontWeight.Bold, color = Color.White)
+                Text(text = "생성하기", fontSize = 14.sp, fontWeight = FontWeight.Bold, color = Color.White)
             }
         }
     }
